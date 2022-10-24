cb525c554251e341a632c15429e39bf8a82c012e
Mary Ring <mring@colgate.edu>
2022-10-24 16:18:11 -0400

    made some methods in controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,35 @@
 class MenusController < ApplicationController
+
+
+
+    def new 
+        @menu = Menu.new
+    end
+
+    def create
+        m = Menu.new(create_update_params)
+        if m.save
+            flash[:notice] = "Menu Item #{m.name} successfully entered"
+            redirect_to menus_path
+        else
+            flash[:warning] = "Menu Item could not be entered"
+            redirect_to new_book_path
+        end
+    end
+
+    def edit
+        @menu = Menu.find params[:id]
+    end 
+
+    def update 
+        @menu = Menu.find params[:id]
+        @menu.update(create_update_params)
+        flash[:notice] = "#{@menu.name} was successfully updated"
+        redirect_to menu_path(@menu)
+    end 
+
+private 
+    def create_update_params
+        params.require(:menu).permit(:name, :description, :meal_of_day, :ingredients, :calories, :allergens, :diet)
+    end
 end
