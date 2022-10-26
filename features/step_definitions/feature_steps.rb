85acdfa9aaaf1c16dae3e2e823f425403f53745a
shaneknopp <sknopp@colgate.edu>
2022-10-26 14:21:04 -0400

    start of cucumber for dininghall/hours

--- features/step_definitions/feature_steps.rb
+++ features/step_definitions/feature_steps.rb
@@ -16,3 +16,21 @@ Then('I should be on the Menu Item show page') do
     expect(page.current_path).to eq(menu_path)
 end
 
+Given('these Dininghalls:') do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    table.hashes.each do |h|
+        Dininghall.create!(h)
+    end
+end
+  
+Given('I am on the index page') do
+  visit root
+end
+  
+When('I click on {string}') do |link|
+    click_link(link)
+end
+
+Then('I should be on the Dininghall show page') do
+  expect(page.current_path).to eq(dininghall_path)
+end
