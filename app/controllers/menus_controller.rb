b82c2080be908f40b2dd0649967251037db5aafc
Mary Ring <mring@colgate.edu>
2022-10-30 22:15:06 -0400

    rubocop, cucumber test for update, simple validate

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,41 +1,41 @@
 class MenusController < ApplicationController
-    def index
-        @menus = Menu.all.order(:meal_of_day)
+  def index
+    @menus = Menu.all.order(:meal_of_day)
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    m = Menu.new(create_update_params)
+    if m.save
+      flash[:notice] = "Menu Item #{m.name} successfully created"
+      redirect_to menus_path
+    else
+      flash[:warning] = 'Menu Item could not be entered'
+      redirect_to new_menu_path
     end
+  end
 
-    def show 
-        @menu = Menu.find(params[:id])
-    end
+  def edit
+    @menu = Menu.find params[:id]
+  end
 
-    def new 
-        @menu = Menu.new
-    end
+  def update
+    @menu = Menu.find params[:id]
+    @menu.update(create_update_params)
+    flash[:notice] = "#{@menu.name} was successfully updated"
+    redirect_to menu_path(@menu)
+  end
 
-    def create
-        m = Menu.new(create_update_params)
-        if m.save
-            flash[:notice] = "Menu Item #{m.name} successfully created"
-            redirect_to menus_path
-        else
-            flash[:warning] = "Menu Item could not be entered"
-            redirect_to new_menu_path
-        end
-    end
+  private
 
-    def edit
-        @menu = Menu.find params[:id]
-    end 
-
-    def update 
-        @menu = Menu.find params[:id]
-        @menu.update(create_update_params)
-        flash[:notice] = "#{@menu.name} was successfully updated"
-        redirect_to menu_path(@menu)
-    end 
-
-private 
-    def create_update_params
-        params.require(:menu).permit(:name, :description, :meal_of_day, :ingredients, :calories, :allergens, :diet)
-    end
-
+  def create_update_params
+    params.require(:menu).permit(:name, :description, :meal_of_day, :ingredients, :calories, :allergens, :diet)
+  end
 end
--- features/step_definitions/feature_steps.rb
+++ features/step_definitions/feature_steps.rb
@@ -1,36 +1,51 @@
 Given(/^these MenuItems:$/) do |table| # table is a Cucumber::Ast::Table
-    table.hashes.each do |h|
-      Menu.create!(h)
-    end
+  table.hashes.each do |h|
+    Menu.create!(h)
+  end
 end
 
 Given('I am on the MenusIndex page') do
-    visit menus_path
+  visit menus_path
 end
 
 When('I click on {string}') do |link|
-    click_link(link)
+  click_link(link)
 end
 
 Then('I should be on the Menu Item show page') do
-    expect(page.current_path).to eq(menu_path)
+  expect(page.current_path).to eq(menu_path)
 end
 
 Given('these Dininghalls:') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    table.hashes.each do |h|
-        Dininghall.create!(h)
-    end
+  # table is a Cucumber::MultilineArgument::DataTable
+  table.hashes.each do |h|
+    Dininghall.create!(h)
+  end
 end
-  
+
 Given('I am on the index page') do
   visit root
 end
-  
+
 When('I click on {string}') do |link|
-    click_link(link)
+  click_link(link)
 end
 
 Then('I should be on the Dininghall show page') do
   expect(page.current_path).to eq(dininghall_path)
+end
+
+Then('I should be on the show menu item page for {string}') do |item|
+  menu_item = Menu.find_by_name(item)
+  expect(page.current_path).to eq(menu_path(menu_item.id))
+end
+
+Then('I should be on the edit menu item page for {string}') do |item|
+  menu_item = Menu.find_by_name(item)
+  expect(page.current_path).to eq(edit_menu_path(menu_item.id))
+end
+
+When('I change the {string} to {string}') do |field, value|
+  # debugger
+  fill_in(field, with: value)
 end
