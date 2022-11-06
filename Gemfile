94ec413e3e2151e73cfd143a8a69dba478a76381
Mary Ring <mring@colgate.edu>
2022-11-06 11:55:11 -0500

    Basic toggling between meals

--- Gemfile
+++ Gemfile
@@ -74,6 +74,6 @@ group :development do
 end
 
 group :production do
-  gem 'pg'
+  #gem 'pg'
 end
 
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,14 @@
 class Menu < ApplicationRecord
   validates :name, presence: true
   validates :calories, numericality: true, comparison: { greater_than_or_equal_to: 0 }
+
+
+  def self.sort_menu_by(meal)
+    if !meal.nil? && meal != "meal_of_day"
+      meal = meal.to_s 
+      Menu.where("meal_of_day = ?", meal)
+    else 
+      Menu.all.order(:meal_of_day)
+    end
+  end
 end
