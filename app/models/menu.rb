a4482a38ee10d2507676d6ad5857226e81ab02b4
Allen Quizon <aquizon@colgate.edu>
2022-11-14 15:25:15 -0500

    menu_hours

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,15 @@ class Menu < ApplicationRecord
   validates :name, presence: true
   validates :calories, numericality: true, comparison: { greater_than_or_equal_to: 0 }
 
-
+  @@hours = {"All_Day" => "7:30am - 12:00am", "Breakfast" => "7:30am - 11:00am", "Lunch" => "11:00am - 4:00pm", "Dinner" => "4:00pm - 12:00am"}
+  def self.get_menu_hours(time_of_day)
+    if (time_of_day.nil?)
+      @@hours["All_Day"]
+    else
+      @@hours[time_of_day]
+    end
+  end
+  
   def self.sort_menu_by(meal, filters)
     query = ""
     if !meal.nil? && meal != "meal_of_day"
