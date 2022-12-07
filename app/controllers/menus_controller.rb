33e4e1d1beb9d5ffa5fbe1b5af5a3ccee300eeb6
accook0 <accook@colgate.edu>
2022-12-06 19:47:36 -0500

    messing around with views

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,14 @@
 class MenusController < ApplicationController
     def index
-        @menus = Menu.all
+        @menus = Dininghall.menus #this needs to be tweaked or associated in seeds
+    end
+
+    def show
+        if (Menu.where(id: params[:id]).empty?)
+            flash[:notice] = "Menu does not exist"
+            redirect_to menus_path
+          else
+            @menu = Menu.find(params[:id])
+          end
     end
 end
