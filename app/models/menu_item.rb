61d50fd3ec468708fec9fd3ef38c3f14d2797e1c
accook0 <accook@colgate.edu>
2022-12-06 21:03:49 -0500

    frick this guy. why does it hate me

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -4,14 +4,14 @@ class MenuItem < ApplicationRecord
   validates :name, presence: true
   validates :calories, numericality: true, comparison: { greater_than_or_equal_to: 0 }
 
-  def self.sort_items_by(meal, filters)
-    query = ""
-    if !meal.nil? && meal != "meal_of_day"
-      meal = meal.to_s 
-      query = MenuItem.where("meal_of_day = ?", meal)
-    else 
-      query =  MenuItem.order(:meal_of_day)
-    end
+  def self.sort_items_by(current_menu, filters)
+    query = Menu.find_by_id(current_menu).menu_items
+    #if !meal.nil? && meal != "meal_of_day"
+    #  meal = meal.to_s 
+    #  query = MenuItem.where("meal_of_day = ?", meal)
+    #else 
+    #  query =  MenuItem.order(:meal_of_day)
+    #end
     filters.each do |filter|
       #debugger
       if filter == "vegan" || filter == "vegetarian"
