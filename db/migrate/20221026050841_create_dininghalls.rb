c9910934fc02bfa1baca6dca5b9453f5c8793fe7
Allen Quizon <aquizon@colgate.edu>
2022-12-08 21:02:17 -0500

    removed time from Dininghall

--- db/migrate/20221026050841_create_dininghalls.rb
+++ db/migrate/20221026050841_create_dininghalls.rb
@@ -3,9 +3,7 @@ class CreateDininghalls < ActiveRecord::Migration[7.0]
     create_table :dininghalls do |t|
       t.text :name
       t.text :hours
-      t.time :time
       t.text :menu
-
       t.timestamps
     end
   end
