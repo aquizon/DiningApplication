214c12e4fa2ff31769ebc551d0270bfddfe74418
shaneknopp <sknopp@colgate.edu>
2022-12-09 10:23:20 -0500

    cucumber works AGAIN and dh controller almost

--- spec/features/create_feature_spec.rb
+++ spec/features/create_feature_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "create page", type: :feature do
     visit dininghalls_path
   end
 
-  it 'should not have a link to create a product if user not signed in' do
+  it 'should not have a link to create a dining hall if user not signed in' do
     sign_out @user
     visit dininghalls_path
     expect(page).not_to have_content('New Dining Hall')
@@ -21,7 +21,7 @@ RSpec.describe "create page", type: :feature do
     expect(page.current_path).to eq(new_user_session_path)
   end
 
-  it 'should successfully create a product if a user is signed in' do
+  it 'should successfully create a dining hall if a user is signed in' do
     click_on 'New Dining Hall'
     fill_in 'Name', with: 'Test Spec'
     fill_in 'Hours', with: '9-5'
@@ -30,15 +30,15 @@ RSpec.describe "create page", type: :feature do
     expect(page).to have_content('Test Spec')
   end
 
-  # it 'should handle failure to create a product gracefully' do
-  #   click_on 'New Dining Hall'
-  #   fill_in 'Name', with: 'Test Spec'
-  #   fill_in 'Hours', with: '9-5'
-  #   d = Dininghall.new
-  #   expect(d).to receive(:save) { nil }
-  #   allow(Dininghall).to receive(:new) { d }
-  #   click_on 'New Dining Hall'
-  #   expect(page.current_path).to eq(new_dininghall_path)
-  #   expect(page).to have_content('Failed to save dining hall')
-  # end
+  it 'should handle failure to create a dining hall gracefully' do
+    click_on 'New Dining Hall'
+    fill_in 'Name', with: 'Test Spec'
+    fill_in 'Hours', with: '9-5'
+    d = Dininghall.new
+    expect(d).to receive(:save) { nil }
+    allow(Dininghall).to receive(:new) { d }
+    click_on 'Create Dining Hall'
+    expect(page).to have_content('Dining hall could not be entered')
+    expect(page.current_path).to eq(new_dininghall_path)
+  end
 end
